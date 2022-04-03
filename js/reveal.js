80d4432d205bd713ade63a0e1a1051e62d340d05
ericparlante <erickparlante@gmail.com>
2022-04-02 23:57:48 -0300

    Animaciones Scroll Reveal

---
+++ js/reveal.js
@@ -0,0 +1,7 @@
+//ScrollReveal().reveal('.introduccion', {delay:2000});
+
+//Descripción
+ScrollReveal().reveal('.foto', { delay: 200 });
+ScrollReveal().reveal('.sobre_mi', { delay: 500 });
+ScrollReveal().reveal('.listas', { delay: 1000 });
+ScrollReveal().reveal('.boton', { delay: 1500 });
